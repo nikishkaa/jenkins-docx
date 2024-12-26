17e7a9091c5e15a00412d7d0926676d429f33e7e
nikita <nikita.goutor@gmail.com>
2024-12-26 18:03:10 +0300

    Init.

---
+++ src/main/java/org/example/jenkindocx/JenkinDocxApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkindocx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDocxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinDocxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jenkindocx/JenkinDocxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jenkindocx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDocxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
